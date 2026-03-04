49687924296b5f8518c2cdbe6232aacfbcc62673
Ukechi Dikio <ukechi.dikio@entelect.co.za>
2026-03-04 22:02:32 +0200

    init devCamp commit

---
+++ back-end-devcamp-stack/java-reference-app-product-service/src/main/java/za.co.entelect.devcamp.productservice/ProductServiceApplication.java
@@ -0,0 +1,7 @@
+package za.co.entelect.devcamp.productservice;
+
+public class ProductServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
